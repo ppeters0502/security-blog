25c48cbe1a8a51c2e11b3fb6833b5232645ff0b8
Pat <pspeters93@gmail.com>
2022-07-10 22:16:51 -0500

    Removed reference that was no longer needed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
 
