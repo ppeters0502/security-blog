8de3a0e3e0cd35890dadf70de535aaeb5f5ff910
Pat Peters <Pat_Peters@gallup.com>
2023-09-27 12:18:32 -0500

    Re-added About and blog links to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,15 +4,14 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">Scream into the void - Today!</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              {/* <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/about">About</Nav.Link>
-              <Nav.Link href="/blog">Blog</Nav.Link> */}
+          <Navbar.Brand href='/'>Scream into the void - Today!</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='me-auto'>
+              <Nav.Link href='/blog/1'>About</Nav.Link>
+              <Nav.Link href='/'>Blog</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
